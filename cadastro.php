ce20cca40d07581a48afd641c991b1b0aae41c19
jorgprado <88862202+jorgprado@users.noreply.github.com>
2022-05-30 23:59:23 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,8 @@
+<?php
+if($_POST){
+	echo "Nome: ".$_POST['nome']. '<br>';
+	echo "Email: ".$_POST['email'].'<br>';
+	echo "Assunto: ".$_POST['assunto']. '<br>';
+	echo "Mensagem: ".$_POST['mensagem']. '<br>';
+		}
+?>
